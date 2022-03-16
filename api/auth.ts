965e4ac76022eb348168155590ef0e87caa0056e
Michael Curran <65498724+MiCurran@users.noreply.github.com>
2022-03-15 20:32:49 -0600

    Update auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -1,81 +1,19 @@
 import { NowRequest, NowResponse } from '@vercel/node'
 
 export const allowList: { [iat: string]: AllowListItem } = {
-  dtinth: {
+  'micurran': {
     publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzToF2nyjDcJIjsYOPUdi
-karObvmc5XjWYoDvlhNhbQmwr7DhXeoIbxY3+cEzXzcS15bGwuXBJNB9I7q5CAjn
-UBaJPB7zZ1kMlSvJdK8WGFCMfZEqaT5s6+CqMRDuAaaKxluHG9jKNecwAd0iZDJK
-SNZIl0qRljHbzrSPvCLHw5BUO4cI8J5CeCOMDYZXB/4icmVSZSe1ynt8mFjcKhAN
-774MCCB/7uzd81MuQSu0l/gDQpiCW61jaEMsmAsI8X3c4bCiprm5/prefdRB8KLl
-sl1ClPODqzkQAPi+tOYyh01Sx6zzY1lkCQHNLt+jAX20dUd/7aDRcns3bq+l4PLl
-/wIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'journal-web': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtucrzfonc9G+AHu915oO
-h4lgwdJ0zMmREh0EZJKBtzlRw9QKW8mCd9GMKVfv4kYOZur9BcDXElzhBsLM09rh
-7HXV2Ba7h7v+KC2bDl2aoeILye9pERLAnSnUWzJxBRwN9VGFp7rxAzFrHwvA1U3o
-yDsBImg2gQMIOzKWgvvd6HE+pQpxEDz336yzEZDs8186XLeIssbC5jht1lb7s3/0
-yoD83zGow/xLirUG/dS/Fzt1O3xK5rBlNrNxRHwv0t36HCRhhPihG62vV7VwnXnI
-43QyQmtHc0m0LNlo6kvGT+jVzPvMjlHHSFYBVzXz7uLqiC6OpYr9R/UkNkaQzAJw
-1wIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'screenshot-redirector': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuQgPix+cRdWGGfHDumA2
-uFfYMhlv2U0Jxc5ww+vIHeDddocxMOXRhjnWkjZ0z6E6Otq8FRgMKXCRFusoXx81
-nmrXrVwschDGf1ypwtaEN0ee5TQ9BCE68Maw8Haafjbwxw7najWiRk9q3VLQVcwC
-Mau0Zi16q/trasqgjXh5AAKUDO+7vsG1FI4yoNHrcVj+zvuLCPy/dcyJp18HpgoR
-bqMiXzkJJRd4vO7yfkW6NBIaudC+nRxui9isQrTAE9oFum5id01g4rvdEqjdFnYJ
-cZbPdanG0q/KbIQn+yddgujGNrrRmGNO14l+6eSMri+pqR+C3JSRU0VOPbguA+hw
-CwIDAQAB
------END PUBLIC KEY-----`,
-  },
-  automatron: {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA44Ry2SLyTOmnd8lhXRal
-NirZiXgDqZf0xy3V23HpO4tnGYO+OaFCz46BQMGKoEhNT3Hp0f3857xufDTMHL6l
-niZbLZUVHOANV53zfyqt/s1GR+JnUkF+yJ4spNvhRWQVw/Uuwr28/D7VT34FB7Pz
-GMZDyKFziwZAEpiZM95kPzU1BBvKuXsRqzVw51BCsOin7rHSjczK4bpBgWxEbtIM
-G4Op7vvNF1fQ6Ww9NTCwQosRfub121XZUBnCEN/7fSIC2Ypz1zWpkCPBL3Ly6mIT
-uX1urElaiBAigqy19aYg5balOFyl3/aw0vUsxG9caetpVp1eKYfVRk8eOPFJ69AS
-KwIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'wonderful.software': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3r3dehkLMOggLWshm6NS
-g2Ct1gtPsi618BaBwPa459xcCvDM9e0wiGujM1kkvyLtEligkoqnOoYZL2YdHfK5
-aR9hBu/3C/uIqa6LMPLP2FO4sOiGaBIIKvxGWvkZ9CzNN/dcBgqSAoMU4UfNd/vH
-NHqJaQ+euTVLPe5JwMtSJqxemREu1wuhAhGI3V8tb0JmYthcvLzpvxeJg1Hwyd1g
-BpU1AatXp1OivGXdHQF4vP69Szn775ZofQzqlXaboCDjJ1Zu9wuvxCzJlplml7C3
-NAFPAKjVtw86bZyH8rMK/mfmh+5VK5gY/QC55ZqV+iA2VazAm/PYb5k9/h3SR27D
-FwIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'webring.wonderful.software': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArCe1g28aEqSrRkucr9/P
-uspJog/kFyGYLQJc2m4FjmWAvnpsGrUJYdzqLEodhAFf6Lpi9M15t4t4kN1ko45o
-VEN+DOdgHp8c3EO76PeScWmg3Y5KS7JUsRY55cNG4mO/rDGwbCTaSA6L1XLz+hSU
-6vefp+y66G5RdS57FnNB9cfsmr2Xc8QxqeSV4ehOkgqEMPc1EYtZnpH1ZLYJa7OP
-zdfGM1Q/FbPhdJpEEYnCJGs0P9+Jkz85Vh12z3AP4WEgjD+v0LDgxLgvALRn1iUp
-CZEQZ9EZ83YnTxQhCyMd7ZQXKcx1kQFkmUPX7FPEMTfJunH5JHywHCXkGgfA6F7v
-4QIDAQAB
------END PUBLIC KEY-----`,
-  },
-  'dtinth-webshots': {
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvgBT7qY48PCtoSILZ9Yq
-48ipyguM14bJqazGaZDYzY+FoXsFXfj/6v/FtuWn0gSiEKZ7Cd+9w256JA7rxCdZ
-ahe9CDYaaHPdglKSg93ncLaLal6KfNzCEaGvCeaKlt7cDNrNkKGg2chZaHKQbV26
-QWzZlSb8xUmNy3AwFd0/H81krCvolRu9zn7UNtXEeDKp7Gmxkr6kWFmWJimt53yg
-ogEnr+5IcxnbMKRn84DrxcFjf3NfjZWZ0lZUXMKHPVyW/ZFGC7lhOWNqwbfyun6B
-JX/9uRg4s1OysojxEks1k4HL5b55n+wnNSXGY+BUe5sum5o8LoEOq74K74cE1mKA
-NwIDAQAB
+AAAAB3NzaC1yc2EAAAADAQABAAACAQDN9N66qqU2arW3CDND+KTB/3jmSDaFZtZKvv
+H8yD5W7Xd4Ed/Uyb1b3oqxFIBtPxA+CN2k9cLoGw8+vLD3MhN9D/DIJWnywk4CKZ29
+KKrOw9M+iykC02zSpSL4HfjxqthrBWvE9Jtf8ULe+FjSQYc/NwiqSEaTPilInyAb37
+trqYpdFefc8xwu3oHFq7U35G7QQiJ2kNfIPmXSkbYiProbsfssfiWoab7JzPbbsuIC
+3sikntOaceoHtqVDEiL7pnu9NW85aB/j8FNvVr5+XUEQ51i+qtjWrag86T71UgElo/
++jQFVvFSIJOg+qvriMYrxhbMP1dLIbzQLn+otMT58fPNNvwPPJcQ0Qe2kzedBeylry
+Ed8rgOhbIEPwr+HkbD7ayF2On2xifiMl9zg7/OcM1J6K9+95IK15FmkSfOkhr+DUZY
+1mghJCwLD8TiSunlR92FPvtNQSGQmg3l7CFKsuowimcyU7jwjk7v1iWIzDxd/thkqj
+7oYvXgoxXMlb5ndhX3gBC83DQ8P0hWDLvPCKxZyGp0DBrSLvrAjJ9il5GIL+qxUxiq
+aVRpiiQuG8gMEP2YQEZ3AGIYvw0h9afTPFeLdL8cwfFAxCdxEfwZyBhYHsU8tr2H2v
+GoWewl1u2OQh5yd0FCso+nB3KHklHiplIWdRZmdva6g33z6uIaKM6Q
 -----END PUBLIC KEY-----`,
   },
 }
